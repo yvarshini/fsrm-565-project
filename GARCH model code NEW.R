67b0952a2a8feb7ddddaaadb0fbac63db0a0ad19
Ckozak003 <129427211+Ckozak003@users.noreply.github.com>
2023-04-30 21:29:29 -0400

    Add files via upload

--- GARCH model code NEW.R
+++ GARCH model code NEW.R
@@ -46,6 +46,7 @@ dccspec1 <- dccspec(uspec = multispec(replicate(2, spec)), dccOrder = c(1,1), di
 dccfit1 <- dccfit(dccspec, data = data1)
 dccfit1
 
+aic <- 16.601
 ##GARCH(1,0)
 spec2 <- ugarchspec(variance.model = list(model = "sGARCH", garchOrder = c(1,0)),
                    mean.model = list(armaOrder=c(0,1,1)), distribution.model = "sstd")
@@ -53,7 +54,7 @@ dccspec2 <- dccspec(uspec = multispec(replicate(2, spec2)), dccOrder = c(1,1), d
 
 dccfit2 <- dccfit(dccspec2, data = data1)
 dccfit2
-
+aic <- c(aic, 16.601)
 #GARCH(0,1)
 spec3 <- ugarchspec(variance.model = list(model = "sGARCH", garchOrder = c(0,1)),
                     mean.model = list(armaOrder=c(0,1,1)), distribution.model = "sstd")
@@ -61,7 +62,7 @@ dccspec3 <- dccspec(uspec = multispec(replicate(2, spec3)), dccOrder = c(1,1), d
 
 dccfit3 <- dccfit(dccspec3, data = data1)
 dccfit3
-
+aic <- c(aic, 17.022)
 ##Trying a skewed-ged with GARCH(1,1):
 spec4 <- ugarchspec(variance.model = list(model = "sGARCH", garchOrder = c(1,1)),
                     mean.model = list(armaOrder=c(0,1,1)), distribution.model = "sged")
@@ -69,14 +70,14 @@ dccspec4 <- dccspec(uspec = multispec(replicate(2, spec4)), dccOrder = c(1,1), d
 
 dccfit4 <- dccfit(dccspec4, data = data1)
 dccfit4
-
+aic <- c(aic, 16.503)
 #GARCH(2,0)
 spec5 <- ugarchspec(variance.model = list(model = "sGARCH", garchOrder = c(2,0)),
                     mean.model = list(armaOrder=c(0,1,1)), distribution.model = "sged")
 dccspec5 <- dccspec(uspec = multispec(replicate(2, spec5)), dccOrder = c(1,1), distribution = "mvt")
-dccspec5
 dccfit5 <- dccfit(dccspec5, data = data1)
 dccfit5
+aic <- c(aic, 16.494)
 #GARCH(0,1)
 spec6 <- ugarchspec(variance.model = list(model = "sGARCH", garchOrder = c(0,1)),
                     mean.model = list(armaOrder=c(0,1,1)), distribution.model = "sged")
@@ -84,6 +85,7 @@ dccspec6 <- dccspec(uspec = multispec(replicate(2, spec6)), dccOrder = c(1,1), d
 
 dccfit6 <- dccfit(dccspec6, data = data1)
 dccfit6
+aic <- c(aic, 23.521)
 
 ### Returns the same AIC value as GARCH(1,1), we shall disregard models
 ### 2-7 as they did not perform as well as model 1 or parsimony dictates
